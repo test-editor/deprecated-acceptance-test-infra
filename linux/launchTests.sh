cd5403abab36cf920b2bede45f971fa37ec9b679
kpanier <kpanier@aysada.org>
2015-08-22 14:14:53 +0200

    TE-246 update platform to mars

--- linux/launchTests.sh
+++ linux/launchTests.sh
@@ -3,5 +3,5 @@
 unzip /vagrant/TestEditor-linux.gtk.x86_64.zip -d /vagrant/testeditor 
 chmod ugo+x /vagrant/testeditor/testeditor
 export DISPLAY=:99
-java -DPATH_TO_TESTEDITOR_AUT=/vagrant/testeditor/testeditor -Daut.workspace.path=/tmp/.testeditor_aut -Dwaits.afterteststep=10ms -jar /vagrant/testeditor/plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -application org.testeditor.core.headlesstestrunner -consoleLog -data /vagrant/ws/test-editor-tests ExecuteTest=TestEditorTests.AceptenceForConDelivery
+java -DPATH_TO_TESTEDITOR_AUT=/vagrant/testeditor/testeditor -Daut.workspace.path=/tmp/.testeditor_aut -Dwaits.afterteststep=10ms -jar /vagrant/testeditor/plugins/org.eclipse.equinox.launcher*.jar -application org.testeditor.core.headlesstestrunner -consoleLog -data /vagrant/ws/test-editor-tests ExecuteTest=TestEditorTests.AceptenceForConDelivery
 
